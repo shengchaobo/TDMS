e730aea483b7923d92cb0fb40d34826c7d8da54f
scb <shengchaobo@126.com>
2014-07-08 20:04:01 +0800

    T612,613,611,614,641,66主要功能
    Signed-off-by: scb <shengchaobo@126.com>

--- src/cn/nit/dao/table6/T66_Dao.java
+++ src/cn/nit/dao/table6/T66_Dao.java
@@ -8,14 +8,13 @@ import java.util.List;
 
 import net.sf.json.JSON;
 
-
-
 import cn.nit.bean.table6.T621_Bean;
 import cn.nit.bean.table6.T622_Bean;
 import cn.nit.bean.table6.T641_Bean;
 import cn.nit.bean.table6.T66_Bean;
 import cn.nit.dbconnection.DBConnection;
 import cn.nit.util.DAOUtil;
+import cn.nit.util.TimeUtil;
 
 public class T66_Dao {
 
@@ -32,259 +31,86 @@ public class T66_Dao {
 	private String fieldShow = "SeqNumber,StuClubSum,StuClubSciNum,StuClubHumanNum,StuClubSportNum,StuClubArtNum,OtherStuClub,JoinStuSum,JoinClubSciNum,StuClubHumanNum1,JoinClubSportNum,JoinClubArtNum,JoinOtherClub,Time,Note";
 
 
-		/* ,FillTeaID,FillUnitID,audit */
-
 	/**
-	 * 将数据表622的实体类插入数据库
-	 * 
-	 * @param UndergraAdmiInfo
+	 * 获取字典表的所有数据
 	 * @return
-	 * 
-	 * @time: 2014-6-12
+	 *
+	 * @time: 2014-5-14/下午02:34:42
 	 */
-	public boolean insert(T66_Bean StuClub) {
-
-		// flag判断数据是否插入成功
-		boolean flag = false;
-		Connection conn = DBConnection.instance.getConnection();
-		try {
-			flag = DAOUtil.insert(StuClub, tableName, field, conn);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return flag;
-		} finally {
+	public T66_Bean getYearInfo(String year){
+		
+		String sql = "select " + " " + key + "," +
+		field + " from " + tableName + " where convert(varchar(4),Time,120)=" + year;
+		Connection conn = DBConnection.instance.getConnection() ;
+		Statement st = null ;
+		ResultSet rs = null ;
+		List<T66_Bean> list = null ;
+		T66_Bean bean = null;
+		try{
+			st = conn.createStatement() ;
+			rs = st.executeQuery(sql) ;
+			list = DAOUtil.getList(rs, T66_Bean.class) ;
+			if(list.size() != 0){
+				bean = list.get(0);
+			}
+		}catch(Exception e){
+			e.printStackTrace() ;
+			return null ;
+		}finally{
 			DBConnection.close(conn);
+			DBConnection.close(rs);
+			DBConnection.close(st);			
 		}
-		return flag;
+		
+		return bean ;
 	}
-
+		
 	/**
-	 * 讲数据批量插入T511表中
-	 * 
-	 * @param list
-	 *            {@linkplain java.util.List<
-	 *            {@link cn.nit.bean.table5.UndergraCSBaseTeaBean}>}
-	 * @return true表示插入成功，false表示插入失败
+	 * 保存数据
+	 * @param diCourseCategories
+	 * @return
+	 *
+	 * @time: 2014-5-14/下午02:34:23
 	 */
-	public boolean batchInsert(List<T66_Bean> list) {
-
+	public boolean save(T66_Bean bean, String year, String fields){
+		
+		String sql = "select * from " + tableName + " where convert(varchar(4),Time,120)=" + year;		
 		boolean flag = false;
-		Connection conn = DBConnection.instance.getConnection();
-
-		try {
-			flag = DAOUtil.batchInsert(list, tableName, field, conn);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return flag;
-		}
-
-		return flag;
-	}
-
-	// 更新
-	public boolean update(T66_Bean StuClub) {
-
-		boolean flag = false;
-		Connection conn = DBConnection.instance.getConnection();
-		try {
-			flag = DAOUtil
-					.update(StuClub, tableName, key, field, conn);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return flag;
-		} finally {
-			DBConnection.close(conn);
-		}
-
-		return flag;
-	}
-
-	// 删除 ids应书写为"(1,2,3)"
-	public boolean deleteItemsByIds(String ids) {
-
-		int flag = 0;
-		StringBuffer sql = new StringBuffer();
-		sql.append("delete from " + tableName);
-		sql.append(" where " + key + " in " + ids);
-		Connection conn = DBConnection.instance.getConnection();
-		Statement st = null;
-
-		try {
-			st = conn.createStatement();
-			flag = st.executeUpdate(sql.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-
-		if (flag == 0) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	public String getTableName() {
-		return this.tableName;
-	}
-	
-	public List<T66_Bean> queryPageList(int pagesize, int currentpage) {
-		// TODO Auto-generated method stub
-		
-		
-		String queryPageSql = "select top " + pagesize + 
-		fieldShow
-		+ " from " + tableName + 
-		" where (SeqNumber not in (select top " + pagesize * (currentpage-1) + " SeqNumber from "+
-		tableName + " order by SeqNumber)) order by SeqNumber" ;
-		System.out.println(queryPageSql);
 		Connection conn = DBConnection.instance.getConnection() ;
+		
 		Statement st = null ;
 		ResultSet rs = null ;
 		List<T66_Bean> list = null ;
-		
-		try{
-			st = conn.createStatement() ;
-			rs = st.executeQuery(queryPageSql) ;
-			list = DAOUtil.getList(rs, T66_Bean.class) ;
-		}catch(Exception e){
-			e.printStackTrace() ;
-			return null ;
-		}finally{
-			DBConnection.close(conn);
-			DBConnection.close(rs);
-			DBConnection.close(st);			
-		}
-		
-		return list ;
-	}
-	
-	public List<T66_Bean> queryPageList(String cond, Object object,
-			int pagesize, int currentpage) {
-		// TODO Auto-generated method stub
-		String queryPageSql;
-		
-			queryPageSql = "select top " + pagesize + 
-			fieldShow
-			+ " from " + tableName + 
-			" where " + cond + " and (SeqNumber not in (select top " + pagesize * (currentpage-1) + " SeqNumber from "+
-			tableName + " where " + cond + " order by SeqNumber)) order by SeqNumber" ;
-	
-
-		System.out.println(queryPageSql);
-		Connection conn = DBConnection.instance.getConnection() ;
-		Statement st = null ;
-		ResultSet rs = null ;
-		List<T66_Bean> list = null ;
-		
-		try{
-			st = conn.createStatement() ;
-			rs = st.executeQuery(queryPageSql) ;
-			list = DAOUtil.getList(rs, T66_Bean.class) ;
-		}catch(Exception e){
-			e.printStackTrace() ;
-			return null ;
-		}finally{
-			DBConnection.close(conn);
-			DBConnection.close(rs);
-			DBConnection.close(st);			
-		}
-		
-		return list ;
-	}
-	
-	public List<T66_Bean> getAllList() {
-		// TODO Auto-generated method stub
-		String sql = "select " + fieldShow + " from " + tableName ;
-		Connection conn = DBConnection.instance.getConnection() ;
-		Statement st = null ;
-		ResultSet rs = null ;
-		List<T66_Bean> list = null ;
-		
+		T66_Bean tempBean = null;
 		try{
 			st = conn.createStatement() ;
 			rs = st.executeQuery(sql) ;
 			list = DAOUtil.getList(rs, T66_Bean.class) ;
+			if(list.size() != 0){
+				tempBean = list.get(0);
+				bean.setSeqNumber(tempBean.getSeqNumber());
+				flag = DAOUtil.update(bean, tableName, key, fields, conn) ;
+			}else{
+				bean.setTime(TimeUtil.changeDateY(year));
+				String tempfields = fields + ",Time";
+				flag = DAOUtil.insert(bean, tableName, tempfields, conn) ;
+			}
 		}catch(Exception e){
 			e.printStackTrace() ;
-			return null ;
+			return false ;
 		}finally{
 			DBConnection.close(conn);
 			DBConnection.close(rs);
 			DBConnection.close(st);			
 		}
-		
-		return list ;
-	}
-	
-	public List<T66_Bean> getAllList(String cond, Object object) {
-		// TODO Auto-generated method stub
-		String sql;
-		
-		sql = "select " + fieldShow + " from " + tableName +" where " + cond;
-	    System.out.println(sql);
-	
-		Connection conn = DBConnection.instance.getConnection() ;
-		Statement st = null ;
-		ResultSet rs = null ;
-		List<T66_Bean> list = null ;
-		
-		try{
-			st = conn.createStatement() ;
-			rs = st.executeQuery(sql) ;
-			list = DAOUtil.getList(rs, T66_Bean.class) ;
-		}catch(Exception e){
-			e.printStackTrace() ;
-			return null ;
-		}finally{
-			DBConnection.close(conn);
-			DBConnection.close(rs);
-			DBConnection.close(st);			
-		}
-		
-		return list ;
-	}
-
-	public static void main(String args[]) {
-
-		T66_Dao StuClubDao = new T66_Dao();
-		T66_Bean StuClub = new T66_Bean();
-//		 StuClub.setSeqNumber(1);
-		//	
-	
-		StuClub.setStuClubSum(87);
-		StuClub.setStuClubSciNum(15);
-		StuClub.setStuClubHumanNum(35);
-		StuClub.setStuClubSportNum(18);
-		StuClub.setStuClubArtNum(19);
-		StuClub.setOtherStuClub(0);
-		StuClub.setJoinStuSum(14725);
-		StuClub.setJoinClubSciNum(1490);
-		StuClub.setStuClubHumanNum1(8005);
-		StuClub.setJoinClubSportNum(2665);
-		StuClub.setJoinClubArtNum(2565);
-		StuClub.setJoinOtherClub(0);
 				
-		StuClub.setTime(new Date());
-		StuClub.setNote("无");
-//		//		
-		StuClubDao.insert(StuClub);
-		//		
-		//	
-		//		
-		// //
-		// System.out.println(underCSBaseTeaDao.auditingData("audit='1'",null,2,10).size())
-		// ;
-		// // System.out.println(StuClubDao.update(StuClub)) ;
-//		 System.out.println(StuClubDao.deleteItemsByIds("(8)")) ;
-
-		System.out.println("success!!");
+		return flag ;
 	}
-
-
-
-
-
-
+	
+	
+	
+	public static void main(String args[]){
+		//T66_Dao testDao =  new T66_Dao() ;
+	}
 
 }

25e8606cd02da3bd0a5c8800ce8a720499128a5b
HuangDan <13120393@bjtu.edu.cn>
2014-07-10 15:54:35 +0800

    修改表S3-1

--- src/cn/nit/action/table3/S31_Action.java
+++ src/cn/nit/action/table3/S31_Action.java
@@ -2,6 +2,7 @@ package cn.nit.action.table3;
 
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Date;
@@ -15,6 +16,7 @@ import org.apache.struts2.ServletActionContext;
 
 
 import cn.nit.bean.other.UserRoleBean;
+import cn.nit.bean.table2.T21_Bean;
 import cn.nit.bean.table3.S31_Bean;
 import cn.nit.dao.table3.S31_DAO;
 import cn.nit.excel.imports.table3.S31Excel;
@@ -24,6 +26,7 @@ import cn.nit.excel.imports.table3.S31Excel;
 
 import cn.nit.pojo.table3.S31POJO;
 import cn.nit.service.table3.S31_Service;
+import cn.nit.util.JsonUtil;
 
 
 public class S31_Action {
@@ -38,7 +41,53 @@ private S31_Service s31_Service = new S31_Service() ;
 	private S31Excel s31Excel = new S31Excel() ;
 	
 
+	/**  哪一年数据  */
+	private String selectYear; //删除的id
+	
+	/**  导出的excelName名 */
+	private String excelName ;
+	
+	/**  前台获数据 */
+	private String data ;
+	
+	//save的字段
+	private String fields;
+	
+	
+	//查询出所有
+	public void loadInfo() throws Exception{
+		
+		HttpServletResponse response = ServletActionContext.getResponse() ;		
+		
+		S31_Bean bean = s31_Service.getYearInfo(this.getSelectYear()) ;
+		
+		//private JSONObject jsonObj;
+		bean.setTime(null);
+		String json = JsonUtil.beanToJson(bean);
+		
+		PrintWriter out = null ;
 
+		if(bean == null){
+			response.setContentType("text/html;charset=UTF-8") ;
+			out = response.getWriter() ;
+			out.println( "<script language='javascript'>window.alert('无该年数据');</script>" ); 
+		}else{
+			try {				
+				System.out.println(json) ;
+				response.setContentType("application/json;charset=UTF-8") ;
+				out = response.getWriter() ;
+				out.print(json) ;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally{
+				if(out != null){
+					out.flush() ;
+					out.close() ;
+				}
+			}
+		}
+	}
 
 	
 
@@ -68,6 +117,9 @@ private S31_Service s31_Service = new S31_Service() ;
 			}
 		}
 	}
+	
+	
+	
 	
 	
 	public InputStream getInputStream(){
@@ -161,6 +213,70 @@ private S31_Service s31_Service = new S31_Service() ;
 	public static void main(String args[]){
 		String match = "[\\d]+" ;
 		System.out.println("23gfhf4".matches(match)) ;
+	}
+
+
+
+
+
+	public void setSelectYear(String selectYear) {
+		this.selectYear = selectYear;
+	}
+
+
+
+
+
+	public String getSelectYear() {
+		return selectYear;
+	}
+
+
+
+
+
+	public void setExcelName(String excelName) {
+		this.excelName = excelName;
+	}
+
+
+
+
+
+	public String getExcelName() {
+		return excelName;
+	}
+
+
+
+
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+
+
+
+
+	public String getData() {
+		return data;
+	}
+
+
+
+
+
+	public void setFields(String fields) {
+		this.fields = fields;
+	}
+
+
+
+
+
+	public String getFields() {
+		return fields;
 	}
 
 }

d6f5dce557be085292fedf03950550b508258109
FanShuangyan <12120410@bjtu.edu.cn>
2014-07-24 18:44:05 +0800

    出差后修改代码

--- src/cn/nit/bean/table1/T13Bean.java
+++ src/cn/nit/bean/table1/T13Bean.java
@@ -4,19 +4,12 @@ import java.util.Date;
 
 public class T13Bean {
 	
-	private int SeqNumber;
 	private String UnitName;
 	private String UnitID;
 	private String Leader;
 	private String TeadID;
-	private Date Time;
 	private String Note;
-	public int getSeqNumber() {
-		return SeqNumber;
-	}
-	public void setSeqNumber(int seqNumber) {
-		SeqNumber = seqNumber;
-	}
+	
 	public String getUnitName() {
 		return UnitName;
 	}
@@ -41,12 +34,7 @@ public class T13Bean {
 	public void setTeadID(String teadID) {
 		TeadID = teadID;
 	}
-	public Date getTime() {
-		return Time;
-	}
-	public void setTime(Date time) {
-		Time = time;
-	}
+	
 	public String getNote() {
 		return Note;
 	}

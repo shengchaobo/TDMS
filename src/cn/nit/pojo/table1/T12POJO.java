7d76b321e60f36e04bcb11bf2f5608ea4ccf19ce
FanShuangyan <12120410@bjtu.edu.cn>
2014-07-25 15:27:17 +0800

    出差后修改表1POJO代码

--- src/cn/nit/pojo/table1/T12POJO.java
+++ src/cn/nit/pojo/table1/T12POJO.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 public class T12POJO {
 	
-	private int SeqNumber;
 	private String UnitID;
 	private String UnitName;
 //	private String UnitType;
@@ -13,15 +12,10 @@ public class T12POJO {
 	/**单位负责人*/
 	private String Leader;
 	private String TeaID;
-	private Date Time;
+	
 	private String Note;
 	
-	public int getSeqNumber() {
-		return SeqNumber;
-	}
-	public void setSeqNumber(int seqNumber) {
-		SeqNumber = seqNumber;
-	}
+	
 	public String getUnitID() {
 		return UnitID;
 	}
@@ -58,12 +52,7 @@ public class T12POJO {
 	public void setTeaID(String teaID) {
 		TeaID = teaID;
 	}
-	public Date getTime() {
-		return Time;
-	}
-	public void setTime(Date time) {
-		Time = time;
-	}
+	
 	public String getNote() {
 		return Note;
 	}

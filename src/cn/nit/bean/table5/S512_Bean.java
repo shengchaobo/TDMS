0c1f9c3e700e063e845bc58cb76827af9375f119
FanShuangyan <12120410@bjtu.edu.cn>
2014-07-25 15:41:19 +0800

    出差后修改表5 Bean的代码

---
+++ src/cn/nit/bean/table5/S512_Bean.java
@@ -0,0 +1,114 @@
+package cn.nit.bean.table5;
+
+import java.util.Date;
+
+public class S512_Bean {
+	
+	private int SeqNumber;
+	private String TeaUnit;
+	private String UnitID;
+	private int SumCS;
+	private int SmallCSNum;
+	private int SumTeaNum;
+	private int QuqlifyTea;
+	private int Professor;
+	private int ViceProfessor;
+	private int JuniorTea;
+	private int JuniorViceProf;
+	private int CSProfNum;
+	private int CSViceProfNum;
+	private Date Time;
+	private String Note;
+	public int getSeqNumber() {
+		return SeqNumber;
+	}
+	public void setSeqNumber(int seqNumber) {
+		SeqNumber = seqNumber;
+	}
+	public String getTeaUnit() {
+		return TeaUnit;
+	}
+	public void setTeaUnit(String teaUnit) {
+		TeaUnit = teaUnit;
+	}
+	public String getUnitID() {
+		return UnitID;
+	}
+	public void setUnitID(String unitID) {
+		UnitID = unitID;
+	}
+	public int getSumCS() {
+		return SumCS;
+	}
+	public void setSumCS(int sumCS) {
+		SumCS = sumCS;
+	}
+	public int getSmallCSNum() {
+		return SmallCSNum;
+	}
+	public void setSmallCSNum(int smallCSNum) {
+		SmallCSNum = smallCSNum;
+	}
+	public int getSumTeaNum() {
+		return SumTeaNum;
+	}
+	public void setSumTeaNum(int sumTeaNum) {
+		SumTeaNum = sumTeaNum;
+	}
+	public int getQuqlifyTea() {
+		return QuqlifyTea;
+	}
+	public void setQuqlifyTea(int quqlifyTea) {
+		QuqlifyTea = quqlifyTea;
+	}
+	public int getProfessor() {
+		return Professor;
+	}
+	public void setProfessor(int professor) {
+		Professor = professor;
+	}
+	public int getViceProfessor() {
+		return ViceProfessor;
+	}
+	public void setViceProfessor(int viceProfessor) {
+		ViceProfessor = viceProfessor;
+	}
+	public int getJuniorTea() {
+		return JuniorTea;
+	}
+	public void setJuniorTea(int juniorTea) {
+		JuniorTea = juniorTea;
+	}
+	public int getJuniorViceProf() {
+		return JuniorViceProf;
+	}
+	public void setJuniorViceProf(int juniorViceProf) {
+		JuniorViceProf = juniorViceProf;
+	}
+	public int getCSProfNum() {
+		return CSProfNum;
+	}
+	public void setCSProfNum(int cSProfNum) {
+		CSProfNum = cSProfNum;
+	}
+	public int getCSViceProfNum() {
+		return CSViceProfNum;
+	}
+	public void setCSViceProfNum(int cSViceProfNum) {
+		CSViceProfNum = cSViceProfNum;
+	}
+	public Date getTime() {
+		return Time;
+	}
+	public void setTime(Date time) {
+		Time = time;
+	}
+	public String getNote() {
+		return Note;
+	}
+	public void setNote(String note) {
+		Note = note;
+	}
+	
+
+}

0305ef6b3be8b16bb745687f40ea4f3afb487e84
HuangDan <13120393@bjtu.edu.cn>
2014-08-19 16:36:04 +0800

    S452

---
+++ src/cn/nit/action/table4/S452_Action.java
@@ -0,0 +1,269 @@
+package cn.nit.action.table4;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jxl.Workbook;
+import jxl.format.Alignment;
+import jxl.format.Border;
+import jxl.format.BorderLineStyle;
+import jxl.format.Colour;
+import jxl.format.UnderlineStyle;
+import jxl.format.VerticalAlignment;
+import jxl.write.Label;
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableFont;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+import net.sf.json.JSON;
+import net.sf.json.JSONSerializer;
+
+import org.apache.struts2.ServletActionContext;
+
+import cn.nit.bean.table4.S452_Bean;
+import cn.nit.dao.table4.S452_Dao;
+import cn.nit.service.table4.S452_Service;
+
+
+public class S452_Action {
+	
+	
+	private S452_Service s452_Service=new S452_Service();
+	
+	private S452_Bean s452_Bean=new S452_Bean();
+	
+	private S452_Dao s452_Dao=new S452_Dao();
+	
+	/**  哪一年数据  */
+	private String selectYear;
+
+	/**  导出的excelName名 */
+	private String excelName ;
+	
+
+	HttpServletResponse response = ServletActionContext.getResponse() ;
+	HttpServletRequest request = ServletActionContext.getRequest() ;
+	
+	//查询出所有
+	
+	public void loadInfo() throws Exception{
+		HttpServletResponse response = ServletActionContext.getResponse() ;		
+		
+		List<S452_Bean> list=s452_Service.getYearInfo(this.getSelectYear());
+		System.out.println(this.getSelectYear());
+		System.out.println(list.size());
+		JSON json = JSONSerializer.toJSON(list) ;
+		PrintWriter out = null ;
+		//System.out.println(json.toString());
+		try {
+			//设置输出内容的格式为json
+			response.setContentType("application/json; charset=UTF-8") ;
+			out = response.getWriter() ;
+			//设置数据的内容的编码格式
+			String outPrint = URLDecoder.decode(json.toString(), "UTF-8") ;
+			out.print(outPrint) ;
+			out.flush() ;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			if(out != null){
+				out.close() ;
+			}
+		}
+	}
+	
+	public InputStream getInputStream() throws IOException{
+		
+
+		System.out.println(this.getSelectYear());
+		List<S452_Bean> list = s452_Dao.totalList(this.getSelectYear());
+		
+	    ByteArrayOutputStream fos = null;
+		
+		if(list.isEmpty()){
+			PrintWriter out = null ;
+			response.setContentType("text/html;charset=utf-8") ;
+			out = response.getWriter() ;
+			out.print("后台传入的数据为空") ;
+			System.out.println("后台传入的数据为空");
+			return null;
+		}else{
+			String sheetName = this.getExcelName();
+						
+		    WritableWorkbook wwb;
+		    try {    
+		           fos = new ByteArrayOutputStream();
+		           wwb = Workbook.createWorkbook(fos);
+		           WritableSheet ws = wwb.createSheet(sheetName , 0);        // 创建一个工作表
+		
+		            //    设置单元格的文字格式
+		           WritableFont wf = new WritableFont(WritableFont.ARIAL,12,WritableFont.BOLD,false,
+		                    UnderlineStyle.NO_UNDERLINE,Colour.BLACK);
+		           WritableCellFormat wcf = new WritableCellFormat(wf);
+		           wcf.setVerticalAlignment(VerticalAlignment.CENTRE);
+		           wcf.setAlignment(Alignment.CENTRE);
+		           wcf.setBorder(Border.ALL, BorderLineStyle.THIN,
+		        		     jxl.format.Colour.BLACK);
+		           ws.setRowView(1, 500);
+		           
+		            //    设置内容单无格的文字格式
+		           WritableFont wf1 = new WritableFont(WritableFont.ARIAL,12,WritableFont.NO_BOLD,false,
+		                    UnderlineStyle.NO_UNDERLINE,Colour.BLACK);
+		            WritableCellFormat wcf1 = new WritableCellFormat(wf1);        
+		            wcf1.setVerticalAlignment(VerticalAlignment.CENTRE);
+		            wcf1.setAlignment(Alignment.CENTRE);
+		            wcf1.setBorder(Border.ALL, BorderLineStyle.THIN,
+			        		     jxl.format.Colour.BLACK);
+		           
+		           ws.addCell(new Label(0, 0, sheetName, wcf)); 
+		           ws.mergeCells(0, 0, 2, 0);
+		           
+		           ws.addCell(new Label(0,2,"序号",wcf));
+		           ws.addCell(new Label(1,2,"教学单位",wcf));
+		           ws.addCell(new Label(2,2,"单位号",wcf));
+		           ws.addCell(new Label(3,2,"1.培训类型",wcf));
+		           ws.addCell(new Label(7,2,"2.培训时长",wcf));
+		           ws.addCell(new Label(10,2,"3.培训地点",wcf));
+		           ws.addCell(new Label(3,3,"到行业培训",wcf));
+		           ws.addCell(new Label(4,3,"攻读博士学位",wcf));
+		           ws.addCell(new Label(5,3,"攻读硕士学位",wcf));
+		           ws.addCell(new Label(6,3,"其他",wcf));
+		           ws.addCell(new Label(7,3,"一个月内",wcf));
+		           ws.addCell(new Label(8,3,"一个月~三个月",wcf));
+		           ws.addCell(new Label(9,3,"三个月以上",wcf));
+		           ws.addCell(new Label(10,3,"境内",wcf));
+		           ws.addCell(new Label(11,3,"境外",wcf));
+		           
+		           ws.addCell(new Label(1,4,list.get(0).getTeaUnit(),wcf1));
+		           ws.addCell(new Label(3,4,list.get(0).getIndustryTrain()+"",wcf1));
+		           ws.addCell(new Label(4,4,list.get(0).getDoctorTrain()+"",wcf1));
+		           ws.addCell(new Label(5,4,list.get(0).getMasterTrain()+"",wcf1));
+		           ws.addCell(new Label(6,4,list.get(0).getOtherTrain()+"",wcf1));
+		           ws.addCell(new Label(7,4,list.get(0).getOneMonth()+"",wcf1));
+		           ws.addCell(new Label(8,4,list.get(0).getOneToThreeMonth()+"",wcf1));
+		           ws.addCell(new Label(9,4,list.get(0).getAboveThreeMonth()+"",wcf1));
+		           ws.addCell(new Label(10,4,list.get(0).getInPlace()+"",wcf1));
+		           ws.addCell(new Label(11,4,list.get(0).getOutPlace()+"",wcf1));
+ 
+		           
+		           for(int i=1;i<list.size();i++){
+		        	   ws.addCell(new Label(0,4+i,i+"",wcf1));
+			           ws.addCell(new Label(1,4+i,list.get(i).getTeaUnit(),wcf1));
+			           ws.addCell(new Label(2,4+i,list.get(i).getUnitID(),wcf1));
+			           ws.addCell(new Label(3,4+i,list.get(i).getIndustryTrain()+"",wcf1));
+			           ws.addCell(new Label(4,4+i,list.get(i).getDoctorTrain()+"",wcf1));
+			           ws.addCell(new Label(5,4+i,list.get(i).getMasterTrain()+"",wcf1));
+			           ws.addCell(new Label(6,4+i,list.get(i).getOtherTrain()+"",wcf1));
+			           ws.addCell(new Label(7,4+i,list.get(i).getOneMonth()+"",wcf1));
+			           ws.addCell(new Label(8,4+i,list.get(i).getOneToThreeMonth()+"",wcf1));
+			           ws.addCell(new Label(9,4+i,list.get(i).getAboveThreeMonth()+"",wcf1));
+			           ws.addCell(new Label(10,4+i,list.get(i).getInPlace()+"",wcf1));
+			           ws.addCell(new Label(11,4+i,list.get(i).getOutPlace()+"",wcf1));
+		           }
+		           
+
+
+
+		           ws.mergeCells(0, 2, 0, 3);
+		           ws.mergeCells(1, 2, 1, 3);
+		           ws.mergeCells(2, 2, 2, 3);
+		           ws.mergeCells(3, 2, 6, 2);
+		           ws.mergeCells(7, 2, 9, 2);
+		           ws.mergeCells(10, 2, 11, 2);
+		           ws.mergeCells(0, 4, 2, 4);
+		           
+
+ 
+		             
+
+		          wwb.write();
+		          wwb.close();
+
+		        } catch (IOException e){
+		        } catch (RowsExceededException e){
+		        } catch (WriteException e){}
+		        
+		}
+		return new ByteArrayInputStream(fos.toByteArray());
+		
+	}
+	
+	
+	
+	public String execute() throws Exception{
+		request.setCharacterEncoding("UTF-8") ;
+		System.out.println("excelName=============" + excelName) ;
+		return "success" ;
+	}
+
+	public String getSelectYear() {
+		return selectYear;
+	}
+
+	public void setSelectYear(String selectYear) {
+		this.selectYear = selectYear;
+	}
+
+	public String getExcelName() {
+		return excelName;
+	}
+
+	public void setExcelName(String excelName) {
+		this.excelName = excelName;
+	}
+
+
+
+
+
+	public S452_Service getS452_Service() {
+		return s452_Service;
+	}
+
+	public void setS452_Service(S452_Service s452Service) {
+		s452_Service = s452Service;
+	}
+
+	public S452_Bean getS452_Bean() {
+		return s452_Bean;
+	}
+
+	public void setS452_Bean(S452_Bean s452Bean) {
+		s452_Bean = s452Bean;
+	}
+
+	public S452_Dao getS452_Dao() {
+		return s452_Dao;
+	}
+
+	public void setS452_Dao(S452_Dao s452Dao) {
+		s452_Dao = s452Dao;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public void setResponse(HttpServletResponse response) {
+		this.response = response;
+	}
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+}
---
+++ src/cn/nit/dao/table4/S452_Dao.java
@@ -0,0 +1,214 @@
+package cn.nit.dao.table4;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.nit.bean.table4.S452_Bean;
+import cn.nit.dbconnection.DBConnection;
+import cn.nit.util.DAOUtil;
+import cn.nit.util.TimeUtil;
+
+public class S452_Dao {
+	
+	
+	
+	
+	/**  数据库表名  */
+	private String tableName = "S452_TeaTrainInfo$" ;
+	
+	/**待统计数据库表名*/
+	private  String tableName1 = "T452_TeaTrainInfo_TeaPer$";
+	
+	
+	private  String tableName2 = "DiDepartment";
+	
+	/**  数据自增长字段的主键，必须为自增长字段  */
+	private String key = "SeqNumber" ;
+	
+	/**  数据库表中除了自增长字段的所有字段  */
+	private String field = "TeaUnit,UnitID,IndustryTrain,DoctorTrain,MasterTrain,OtherTrain," +
+			"OneMonth,OneToThreeMonth,AboveThreeMonth,InPlace,OutPlace,Time,Note" ;
+
+	
+
+    /**
+	 * 获取字典表的所有数据
+	 * @return
+	 *
+	 * @time: 2014-5-14/下午02:34:42
+	 */
+	public List<S452_Bean> totalList(String year){
+		
+		String sql = "select " + key+ "," +field + " from " + tableName 
+				+ " where convert(varchar(4),Time,120)=" + year;		
+		Connection conn = DBConnection.instance.getConnection() ;
+		Statement st = null ;
+		ResultSet rs = null ;
+		List<S452_Bean> list = null ;
+		System.out.println(sql);
+		try{
+			st = conn.createStatement() ;
+			rs = st.executeQuery(sql) ;
+			list = DAOUtil.getList(rs, S452_Bean.class) ;
+		}catch(Exception e){
+			e.printStackTrace() ;
+			return null ;
+		}finally{
+			DBConnection.close(conn);
+			DBConnection.close(rs);
+			DBConnection.close(st);			
+		}
+		
+		return list ;
+	}
+	
+  /**
+ 	 * 模板导入
+ 	 * @param diCourseCategories
+ 	 * @return
+ 	 *
+ 	 * @time: 2014-5-14/下午02:34:23
+ 	 */
+ 	public boolean save(List<S452_Bean> list,String year){
+ 		
+		String sql = "select * from " + tableName + " where convert(varchar(4),Time,120)=" + year;		
+		boolean flag = false;
+		Connection conn = DBConnection.instance.getConnection() ;		
+		Statement st = null ;
+		ResultSet rs = null ;
+		List<S452_Bean> templist = null ;
+		
+		try{
+			st = conn.createStatement() ;
+			rs = st.executeQuery(sql) ;
+			templist = DAOUtil.getList(rs, S452_Bean.class) ;
+			if(templist.size() != 0){
+				String delSql = "delete from " + tableName + " where convert(varchar(4),Time,120)=" + year;
+				int delflag = st.executeUpdate(delSql.toString());
+				if(delflag > 0 ){
+					flag = DAOUtil.batchInsert(list, tableName, field, conn) ;
+				}
+			}else{
+				flag = DAOUtil.batchInsert(list, tableName, field, conn) ;
+			}
+		}catch(Exception e){
+			e.printStackTrace() ;
+			return false ;
+		}finally{
+			DBConnection.close(conn);
+			DBConnection.close(rs);
+			DBConnection.close(st);			
+		}
+ 		
+ 		return flag ;
+ 		
+ 	}
+	/**得到数据*/
+
+	public List<S452_Bean> getYearInfo(String year)
+	{
+		
+	String querysql="select "+tableName2+".UnitName as TeaUnit,"+tableName2+".UnitID as UnitID," +
+	"sum(case when "+tableName1+".TrainType = '到行业培训' then 1 else 0 end) as IndustryTrain," +
+	"sum(case when "+tableName1+".TrainType = '攻读博士学位' then 1 else 0 end) as DoctorTrain," +
+	"sum(case when "+tableName1+".TrainType = '攻读硕士学位' then 1 else 0 end) as MasterTrain," +
+	"sum(case when "+tableName1+".TrainType = '其他' then 1 else 0 end) as OtherTrain," +
+	"sum(case when DateDiff(day,"+tableName1+".BeginTime ,"+tableName1+".EndTime)<30 then 1 else 0 end) as OneMonth," +
+	"sum(case when DateDiff(day,"+tableName1+".BeginTime ,"+tableName1+".EndTime)>=30 and DateDiff(day,"+tableName1+".BeginTime ,"+tableName1+".EndTime)<=90 then 1 else 0 end) as OneToThreeMonth," +
+	"sum(case when DateDiff(day,"+tableName1+".BeginTime ,"+tableName1+".EndTime)>90 then 1 else 0 end) as AboveThreeMonth," +
+	"sum(case when "+tableName1+".InOrOut = '境内' then 1 else 0 end) as InPlace," +
+	"sum(case when "+tableName1+".InOrOut = '境外' then 1 else 0 end) as OutPlace" +	
+	" from "+tableName1+
+	" right join "+tableName2+" on "+tableName2+".UnitID = "+tableName1+".UnitID "+
+	" and Time like '"+year+"%' where "+tableName2+".UnitID like '3%' group by "+tableName2+".UnitName,"+tableName2+".UnitID " +
+	" order by "+tableName2+".UnitID";
+   
+		System.out.println(querysql);
+		Connection conn = DBConnection.instance.getConnection() ;
+		Statement st = null ;
+		ResultSet rs = null ;
+		List<S452_Bean> list = new ArrayList<S452_Bean>() ;
+		
+		int sum1=0,sum2=0,sum3=0,sum4=0,sum5=0,sum6=0,sum7=0,sum8=0,sum9=0;
+		int num1=0,num2=0,num3=0,num4=0,num5=0,num6=0,num7=0,num8=0,num9=0;
+		
+		try{
+			st = conn.createStatement() ;
+			rs = st.executeQuery(querysql) ;
+			while(rs.next()){
+				
+				num1 = rs.getInt("IndustryTrain");
+				num2 = rs.getInt("DoctorTrain");
+				num3 = rs.getInt("MasterTrain");
+				num4 = rs.getInt("OtherTrain");
+				num5 = rs.getInt("OneMonth");
+				num6 = rs.getInt("OneToThreeMonth");
+				num7 = rs.getInt("AboveThreeMonth");
+				num8 = rs.getInt("InPlace");
+				num9 = rs.getInt("OutPlace");
+				
+				sum1 += num1;
+				sum2 += num2;
+				sum3 += num3;
+				sum4 += num4;
+				sum5 += num5;
+				sum6 += num6;
+				sum7 += num7;
+				sum8 += num8;
+				sum9 += num9;
+
+
+				
+				S452_Bean s452_Bean=new S452_Bean();
+				s452_Bean.setTeaUnit(rs.getString("TeaUnit"));
+				s452_Bean.setUnitID(rs.getString("UnitID"));
+				s452_Bean.setIndustryTrain(num1);
+				s452_Bean.setDoctorTrain(num2);
+				s452_Bean.setMasterTrain(num3);
+				s452_Bean.setOtherTrain(num4);
+				s452_Bean.setOneMonth(num5);
+				s452_Bean.setOneToThreeMonth(num6);
+				s452_Bean.setAboveThreeMonth(num7);
+				s452_Bean.setInPlace(num8);
+				s452_Bean.setOutPlace(num9);	
+				s452_Bean.setTime(TimeUtil.changeDateY(year));								
+				list.add(s452_Bean);
+				
+			}
+			
+			if(list.size()!=0){
+				S452_Bean s452_Bean=new S452_Bean();
+				s452_Bean.setTeaUnit("合计");
+				s452_Bean.setUnitID(null);			
+				s452_Bean.setIndustryTrain(sum1);
+				s452_Bean.setDoctorTrain(sum2);
+				s452_Bean.setMasterTrain(sum3);
+				s452_Bean.setOtherTrain(sum4);
+				s452_Bean.setOneMonth(sum5);
+				s452_Bean.setOneToThreeMonth(sum6);
+				s452_Bean.setAboveThreeMonth(sum7);
+				s452_Bean.setInPlace(sum8);
+				s452_Bean.setOutPlace(sum9);	
+				s452_Bean.setTime(TimeUtil.changeDateY(year));	
+				list.add(0,s452_Bean);
+				
+				S452_Dao s452_Dao = new S452_Dao();
+				s452_Dao.save(list, year);
+			}		
+		}catch(Exception e){
+			e.printStackTrace() ;
+			return null;
+		}
+		
+		return list ;
+	}
+	
+
+	
+
+	
+
+}

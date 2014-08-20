32512c69f92c111c3f4a6bd200284d39f8e8c20c
liuHY <11112096@bjtu.edu.cn>
2014-08-20 16:48:16 +0800

    职称级别表管理

--- src/cn/nit/action/di/DiTitleLevelAction.java
+++ src/cn/nit/action/di/DiTitleLevelAction.java
@@ -11,8 +11,9 @@ import net.sf.json.JSONSerializer;
 
 import org.apache.struts2.ServletActionContext;
 
+
 import cn.nit.bean.di.DiTitleLevelBean;
-import cn.nit.dao.table4.T411_Dao;
+import cn.nit.bean.di.DiTitleLevelBean;
 import cn.nit.service.di.DiTitleLevelService;
 
 
@@ -20,6 +21,8 @@ public class DiTitleLevelAction {
 
 	HttpServletResponse response = ServletActionContext.getResponse();
 	private DiTitleLevelService TitleLevelServices = new DiTitleLevelService() ;
+	private DiTitleLevelBean level_bean = new DiTitleLevelBean();
+	private String ids ;
 	//查出所有
 	public void loadTitleLevel() throws Exception{
 		
@@ -43,6 +46,126 @@ public class DiTitleLevelAction {
 				out.close() ;
 			}
 		}
+	}
+	
+	
+public void edit(){
+		
+		boolean flag = TitleLevelServices.update(level_bean) ;
+		
+		PrintWriter out = null ;
+		
+		try{
+			
+			getResponse().setContentType("text/html; charset=UTF-8") ;
+			out = getResponse().getWriter() ;
+			if(flag){
+				out.print("{\"state\":true,data:\"更新成功!!!\"}") ;
+			}else{
+				out.print("{\"state\":true,data:\"更新失败!!!\"}") ;
+			}
+			out.flush() ;
+		}catch(Exception e){
+			e.printStackTrace() ;
+			out.print("{\"state\":false,data:\"系统错误，请联系管理员!!!\"}") ;
+		}finally{
+			if(out != null){
+				out.close() ;
+			}
+		}
+	}
+	
+	/**
+	 * 添加用户
+	 */
+	public void insert(){
+		
+		//首先该用 户是否已存数据库
+		Boolean flag0 = TitleLevelServices.hasLevel(level_bean.getIndexId());
+		
+		boolean flag = false;
+		//如果该用户不存数据库，可以添加
+		if(flag0 == false){			
+			flag = TitleLevelServices.insert(level_bean) ;
+		}
+		
+		PrintWriter out = null ;
+		
+		try{
+			
+			getResponse().setContentType("text/html; charset=UTF-8") ;
+			out = getResponse().getWriter() ;
+			
+			if(flag0){
+				out.print("{\"state\":true,data:\"该职称级别已存在!!!\"}") ;
+			}else{
+				if(flag){
+					out.print("{\"state\":true,data:\"添加成功!!!\"}") ;
+				}else{
+					out.print("{\"state\":false,data:\"添加失败!!!\"}") ;
+				}
+			}
+
+			
+		}catch(Exception e){
+			e.printStackTrace() ;
+			out.print("{\"state\":false,data:\"添加失败!!!\"}") ;
+		}finally{
+			out.flush() ;
+			
+			if(out != null){
+				out.close() ;
+			}
+		}
+	}
+	
+	/**
+	 * 删除用户
+	 */
+	public void deleteByIds(){
+		
+		boolean flag = TitleLevelServices.deleteByIds(ids) ;
+		PrintWriter out = null ;
+		
+		try{
+			getResponse().setContentType("text/html; charset=UTF-8") ;
+			out = getResponse().getWriter() ;
+			
+			if(flag){
+				out.print("{\"state\":true,data:\"删除成功!!!\"}") ;
+			}else{
+				out.print("{\"state\":false,data:\"删除失败!!!\"}") ;
+			}
+			
+			out.flush() ;
+		}catch(Exception e){
+			e.printStackTrace() ;
+			out.print("{\"state\":false,data:\"系统错误，请联系管理员!!!\"}") ;
+		}finally{
+			if(out != null){
+				out.close() ;
+			}
+		}
+	}
+	
+	public void setLevel_bean(DiTitleLevelBean level_bean) {
+		this.level_bean = level_bean;
+	}
+
+	public DiTitleLevelBean getLevel_bean() {
+		return level_bean;
+	}
+	
+	public HttpServletResponse getResponse(){
+		return ServletActionContext.getResponse() ;
+	}
+	
+	public String getIds() {
+		return ids;
+	}
+
+	public void setIds(String ids) {
+		this.ids = ids;
 	}
 
 }

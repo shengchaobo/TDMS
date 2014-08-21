87ad9ce97dc19e09a4700e9ec5ef1ad70d8a7df7
JuZhuoya <JuZhuoya@gmail.com>
2014-08-21 16:53:32 +0800

    file name update p2

---
+++ src/cn/nit/bean/di/DiUserRoleBean.java
@@ -0,0 +1,26 @@
+package cn.nit.bean.di;
+
+public class DiUserRoleBean {
+
+	private String TeaID ;
+	
+	private String RoleID ;
+
+	public String getTeaID() {
+		return TeaID;
+	}
+
+	public void setTeaID(String teaID) {
+		TeaID = teaID;
+	}
+
+	public String getRoleID() {
+		return RoleID;
+	}
+
+	public void setRoleID(String roleID) {
+		RoleID = roleID;
+	}
+	
+	
+}

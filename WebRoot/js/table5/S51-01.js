d2f04b63405dd2ce2270e072c2db0872b1e3fe7d
FanShuangyan <12120410@bjtu.edu.cn>
2014-08-27 15:10:33 +0800

    修改s71.js的小bug

--- WebRoot/js/table5/S51-01.js
+++ WebRoot/js/table5/S51-01.js
@@ -14,13 +14,15 @@
 			collapsible : true,//显示可折叠按钮
 			toolbar : "#toolbar",//在添加 增添、删除、修改操作的按钮要用到这个
 			singleSelect : false,//为true时只能选择单行
-			fitColumns : false,//允许表格自动缩放，以适应父容器
+			fitColumns : true,//允许表格自动缩放，以适应父容器
 			//sortName : 'xh',//当数据表格初始化时以哪一列来排序
 			//sortOrder : 'desc',//定义排序顺序，可以是'asc'或者'desc'（正序或者倒序）。
 			remoteSort : false,
 			rownumbers : true,
+			async : false,
+			type : "POST",
 			onLoadSuccess: function (rowData) {
-					 
+
 			if(typeof(rowData.rows[0].data) != "undefined"){
 				alert(rowData.rows[0].data);
 			}
@@ -48,7 +50,7 @@
 		
 		
 		//刷新页面
-		 $("#cbYearContrast").combobox({  
+		 $("#cbYearContrast").combobox({
 	        onChange:function(newValue, oldValue){  
 				reloadgrid(newValue);
 	        }
@@ -57,11 +59,11 @@
 		//查询
 		function reloadgrid (year)  { 
 			
-			//alert(year);
+//			alert(year);
 	        //查询参数直接添加在queryParams中 
 //			alert(222);
 	         var queryParams = $('#showData').datagrid('options').queryParams;  
-	         queryParams.selectYear = year;   
+	         queryParams.selectYear = year; 
 	         $("#showData").datagrid('reload'); 
 	    }	
 		

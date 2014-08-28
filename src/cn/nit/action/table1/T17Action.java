a0c4349e320b4ca15bcc6e69e4f66a4f7386c60d
FanShuangyan <12120410@bjtu.edu.cn>
2014-08-28 16:46:15 +0800

    修改1表导出excelname乱码问题

--- src/cn/nit/action/table1/T17Action.java
+++ src/cn/nit/action/table1/T17Action.java
@@ -3,6 +3,8 @@ package cn.nit.action.table1;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -40,6 +42,12 @@ public class T17Action {
 	private String excelName; //
 	
 	public String getExcelName() {
+		try {
+			this.excelName = URLEncoder.encode(excelName, "UTF-8");
+			//this.saveFile = new String(saveFile.getBytes("ISO-8859-1"),"UTF-8");// 中文乱码解决
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 		return excelName;
 	}
 
@@ -270,7 +278,7 @@ public class T17Action {
 			
 			List<T17Bean> list = t17Dao.totalList();
 			
-			String sheetName = this.getExcelName();
+			String sheetName = this.excelName;
 			
 			List<String> columns = new ArrayList<String>();
 			columns.add("序号");

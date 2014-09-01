16ceae327e77c54bb12fd1a1d322e5eb30080d1a
HuangDan <13120393@bjtu.edu.cn>
2014-09-01 20:30:15 +0800

    添加A323

---
+++ src/cn/nit/action/table3/A323_Action.java
@@ -0,0 +1,284 @@
+package cn.nit.action.table3;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jxl.Workbook;
+import jxl.format.Alignment;
+import jxl.format.Border;
+import jxl.format.BorderLineStyle;
+import jxl.format.Colour;
+import jxl.format.UnderlineStyle;
+import jxl.format.VerticalAlignment;
+import jxl.write.Label;
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableFont;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+import net.sf.json.JSON;
+import net.sf.json.JSONSerializer;
+
+import org.apache.struts2.ServletActionContext;
+
+import cn.nit.bean.table3.A323_Bean;
+import cn.nit.dao.table3.A323_DAO;
+import cn.nit.service.table3.A323_Service;
+
+public class A323_Action {
+	
+	
+	
+	private A323_Service a323_Service=new A323_Service();
+	
+	private A323_Bean a323_Bean=new A323_Bean();
+	
+	private A323_DAO a323_Dao=new A323_DAO();
+	
+	/**  哪一年数据  */
+	private String selectYear;
+
+	/**  导出的excelName名 */
+	private String excelName ;
+	
+
+	HttpServletResponse response = ServletActionContext.getResponse() ;
+	HttpServletRequest request = ServletActionContext.getRequest() ;
+	
+	//查询出所有
+	
+	public void loadInfo() throws Exception{
+		HttpServletResponse response = ServletActionContext.getResponse() ;		
+		
+		List<A323_Bean> list = a323_Service.getYearInfo(this.getSelectYear());		
+		boolean flag = true;
+		JSON json = null;
+		if(list.size()==0){
+			flag = false;
+		}else{
+			 json = JSONSerializer.toJSON(list) ;
+			 System.out.println(json.toString());
+		}		
+		PrintWriter out = null ;		
+		try {
+			
+			if(flag){
+				//设置输出内容的格式为json
+				response.setContentType("application/json; charset=UTF-8") ;				
+				out = response.getWriter() ;
+				//设置数据的内容的编码格式
+				String outPrint = URLDecoder.decode(json.toString(), "UTF-8") ;
+				out.print(outPrint) ;
+			}else{
+				response.setContentType("text/html; charset=UTF-8") ;
+				out = response.getWriter() ;
+				out.print("[{\"data\":\"该统计表数据不全，请填写相关数据后再进行统计!!!\"}]") ;
+				System.out.println("统计数据不全");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			if(out != null){
+				out.flush() ;
+				out.close() ;
+			}
+		}
+	}
+	
+	
+//	public InputStream getInputStream() throws IOException{
+//		
+//
+//		System.out.println(this.getSelectYear());
+//		List<A323_Bean> list = a323_Dao.totalList(this.getSelectYear());
+//		
+//	    ByteArrayOutputStream fos = null;
+//		
+//		if(list.isEmpty()){
+//			PrintWriter out = null ;
+//			response.setContentType("text/html;charset=utf-8") ;
+//			out = response.getWriter() ;
+//			out.print("后台传入的数据为空") ;
+//			System.out.println("后台传入的数据为空");
+//			return null;
+//		}else{
+//			String sheetName = this.excelName;
+//						
+//		    WritableWorkbook wwb;
+//		    try {    
+//		           fos = new ByteArrayOutputStream();
+//		           wwb = Workbook.createWorkbook(fos);
+//		           WritableSheet ws = wwb.createSheet(sheetName, 0);        // 创建一个工作表
+//		
+//		            //    设置单元格的文字格式
+//		           WritableFont wf = new WritableFont(WritableFont.ARIAL,12,WritableFont.BOLD,false,
+//		                    UnderlineStyle.NO_UNDERLINE,Colour.BLACK);
+//		           WritableCellFormat wcf = new WritableCellFormat(wf);
+//		           wcf.setVerticalAlignment(VerticalAlignment.CENTRE);
+//		           wcf.setAlignment(Alignment.CENTRE);
+//		           wcf.setBorder(Border.ALL, BorderLineStyle.THIN,
+//		        		     jxl.format.Colour.BLACK);
+//		           ws.setRowView(1, 500);
+//		           
+//		            //    设置内容单无格的文字格式
+//		           WritableFont wf1 = new WritableFont(WritableFont.ARIAL,12,WritableFont.NO_BOLD,false,
+//		                    UnderlineStyle.NO_UNDERLINE,Colour.BLACK);
+//		            WritableCellFormat wcf1 = new WritableCellFormat(wf1);        
+//		            wcf1.setVerticalAlignment(VerticalAlignment.CENTRE);
+//		            wcf1.setAlignment(Alignment.CENTRE);
+//		            wcf1.setBorder(Border.ALL, BorderLineStyle.THIN,
+//			        		     jxl.format.Colour.BLACK);
+//		           
+//		           ws.addCell(new Label(0, 0, sheetName, wcf)); 
+//		           ws.mergeCells(0, 0, 3, 0);
+//		           
+//		           ws.addCell(new Label(0, 2, "序号", wcf)); 
+//		           ws.addCell(new Label(1, 2, "教学单位", wcf)); 
+//		           ws.addCell(new Label(2, 2, "单位号", wcf)); 
+//		           ws.addCell(new Label(3, 2, "本科专业数（个）", wcf)); 
+//		           ws.addCell(new Label(4, 2, "优势专业占专业总数比例（%）", wcf)); 
+//		           ws.addCell(new Label(4, 3, "合计", wcf)); 
+//		           ws.addCell(new Label(5, 3, "国际级", wcf)); 
+//		           ws.addCell(new Label(6, 3, "国家级", wcf)); 
+//		           ws.addCell(new Label(7, 3, "省部级", wcf)); 
+//		           ws.addCell(new Label(8, 3, "市级", wcf)); 
+//		           ws.addCell(new Label(9, 3, "校级", wcf)); 
+//		           ws.addCell(new Label(0, 4, "全校合计", wcf)); 
+//		           ws.addCell(new Label(3, 4, ""+list.get(0).getFieldNum(), wcf1)); 
+//		           ws.addCell(new Label(4, 4, ""+list.get(0).getSum()+"%", wcf1)); 
+//		           ws.addCell(new Label(5, 4, ""+list.get(0).getInternationRatio()+"%", wcf1)); 
+//		           ws.addCell(new Label(6, 4, ""+list.get(0).getNationRatio()+"%", wcf1)); 
+//		           ws.addCell(new Label(7, 4, ""+list.get(0).getProviRatio()+"%", wcf1)); 
+//		           ws.addCell(new Label(8, 4, ""+list.get(0).getCityRatio()+"%", wcf1)); 
+//		           ws.addCell(new Label(9, 4, ""+list.get(0).getSchoolRatio()+"%", wcf1));  
+//		           
+//		           for(int i=1;i<list.size();i++){
+//		        	   ws.addCell(new Label(0, 4+i,""+i, wcf));
+//		        	   ws.addCell(new Label(1, 4+i,list.get(i).getTeaUnit(), wcf));
+//		        	   ws.addCell(new Label(2, 4+i,""+list.get(i).getUnitID(), wcf1));
+//		        	   ws.addCell(new Label(3, 4+i,""+list.get(i).getFieldNum(), wcf1));
+//		        	   ws.addCell(new Label(4, 4+i,""+list.get(i).getSum()+"%", wcf1));
+//		        	   ws.addCell(new Label(5, 4+i,""+list.get(i).getInternationRatio()+"%", wcf1));
+//		        	   ws.addCell(new Label(6, 4+i,""+list.get(i).getNationRatio()+"%", wcf1));
+//		        	   ws.addCell(new Label(7, 4+i,""+list.get(i).getProviRatio()+"%", wcf1));
+//		        	   ws.addCell(new Label(8, 4+i,""+list.get(i).getCityRatio()+"%", wcf1));
+//		        	   ws.addCell(new Label(9, 4+i,""+list.get(i).getSchoolRatio()+"%", wcf1));
+//		           }
+//		           
+//
+//
+//
+//		           ws.mergeCells(0, 2, 0, 3);
+//		           ws.mergeCells(1, 2, 1, 3);
+//		           ws.mergeCells(2, 2, 2, 3);
+//		           ws.mergeCells(3, 2, 3, 3);
+//		           ws.mergeCells(4, 2, 9, 2);
+//		           ws.mergeCells(0, 4, 2, 4);
+//		           
+//
+// 
+//		             
+//
+//		          wwb.write();
+//		          wwb.close();
+//
+//		        } catch (IOException e){
+//		        } catch (RowsExceededException e){
+//		        } catch (WriteException e){}
+//		        
+//		}
+//		return new ByteArrayInputStream(fos.toByteArray());
+//		
+//	}
+	
+
+	
+	public String execute() throws Exception{
+		request.setCharacterEncoding("UTF-8") ;
+		System.out.println("excelName=============" + this.excelName) ;
+		return "success" ;
+	}
+
+	public String getSelectYear() {
+		return selectYear;
+	}
+
+	public void setSelectYear(String selectYear) {
+		this.selectYear = selectYear;
+	}
+
+	public String getExcelName() {
+		try {
+			this.excelName = URLEncoder.encode(excelName, "UTF-8");
+			//this.saveFile = new String(saveFile.getBytes("ISO-8859-1"),"UTF-8");// 中文乱码解决
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return excelName;
+	}
+
+	public void setExcelName(String excelName) {
+		this.excelName = excelName;
+	}
+
+	
+
+	public A323_Service getA323_Service() {
+		return a323_Service;
+	}
+
+
+	public void setA323_Service(A323_Service a323Service) {
+		a323_Service = a323Service;
+	}
+
+
+	public A323_Bean getA323_Bean() {
+		return a323_Bean;
+	}
+
+
+	public void setA323_Bean(A323_Bean a323Bean) {
+		a323_Bean = a323Bean;
+	}
+
+
+	public A323_DAO getA323_Dao() {
+		return a323_Dao;
+	}
+
+
+	public void setA323_Dao(A323_DAO a323Dao) {
+		a323_Dao = a323Dao;
+	}
+
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public void setResponse(HttpServletResponse response) {
+		this.response = response;
+	}
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+}
---
+++ src/cn/nit/dao/table3/A323_DAO.java
@@ -0,0 +1,280 @@
+package cn.nit.dao.table3;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.nit.bean.table3.A323_Bean;
+import cn.nit.dbconnection.DBConnection;
+import cn.nit.util.DAOUtil;
+import cn.nit.util.TimeUtil;
+
+public class A323_DAO {
+	
+	
+	
+	/**  数据库表名  */
+	private String tableName = "A323_HourCreditAnaly" ;
+	
+	/**待统计数据库表名*/
+
+	private static String tableName1 = "T322_UndergraMajorInfo_Tea$";
+	
+	/**  数据自增长字段的主键，必须为自增长字段  */
+	private String key = "SeqNumber" ;
+	
+	/**  数据库表中除了自增长字段的所有字段  */
+	private String field = "TeaUnit,UnitID,RequireHour,OptionHour,InClassHour,ExpHour,PraHour,RequireCredit,OptionCredit,InClassCredit," +
+			"ExpCredit,PraCredit,OutClassCredit,Time,Note" ;
+
+	
+	
+    /**
+	 * 获取字典表的所有数据
+	 * @return
+	 *
+	 * @time: 2014-5-14/下午02:34:42
+	 */
+	public List<A323_Bean> totalList(String year){
+		
+		String sql = "select " + key+ "," +field + " from " + tableName 
+				+ " where convert(varchar(4),Time,120)=" + year;		
+		Connection conn = DBConnection.instance.getConnection() ;
+		Statement st = null ;
+		ResultSet rs = null ;
+		List<A323_Bean> list = null ;
+		System.out.println(sql);
+		try{
+			st = conn.createStatement() ;
+			rs = st.executeQuery(sql) ;
+			list = DAOUtil.getList(rs, A323_Bean.class) ;
+		}catch(Exception e){
+			e.printStackTrace() ;
+			return null ;
+		}finally{
+			DBConnection.close(conn);
+			DBConnection.close(rs);
+			DBConnection.close(st);			
+		}
+		
+		return list ;
+	}
+	
+  /**
+ 	 * 模板导入
+ 	 * @param diCourseCategories
+ 	 * @return
+ 	 *
+ 	 * @time: 2014-5-14/下午02:34:23
+ 	 */
+ 	public boolean save(List<A323_Bean> list,String year){
+ 		
+		String sql = "select * from " + tableName + " where convert(varchar(4),Time,120)=" + year;		
+		boolean flag = false;
+		Connection conn = DBConnection.instance.getConnection() ;		
+		Statement st = null ;
+		ResultSet rs = null ;
+		List<A323_Bean> templist = null ;
+		
+		try{
+			st = conn.createStatement() ;
+			rs = st.executeQuery(sql) ;
+			templist = DAOUtil.getList(rs, A323_Bean.class) ;
+			if(templist.size() != 0){
+				String delSql = "delete from " + tableName + " where convert(varchar(4),Time,120)=" + year;
+				int delflag = st.executeUpdate(delSql.toString());
+				if(delflag > 0){
+					flag = DAOUtil.batchInsert(list, tableName, field, conn) ;
+				}
+			}else{
+				flag = DAOUtil.batchInsert(list, tableName, field, conn) ;
+			}
+		}catch(Exception e){
+			e.printStackTrace() ;
+			return false ;
+		}finally{
+			DBConnection.close(conn);
+			DBConnection.close(rs);
+			DBConnection.close(st);			
+		}
+ 		
+ 		return flag ;
+ 		
+ 	}
+	/**得到数据*/
+
+	public List<A323_Bean> getData(String year)
+	{
+		
+
+		Connection conn = DBConnection.instance.getConnection() ;
+		Statement st = null ;
+		ResultSet rs = null ;
+		
+		String sql = " select DiDepartment.UnitName as TeaUnit, count("+tableName1+".MajorName) as majorNum from DiDepartment right join " +
+				""+tableName1+" on DiDepartment.UnitID = "+tableName1+".FillUnitID and Time like '"+year+"%' " +
+						" where "  +  "DiDepartment.UnitID like '3%' group by DiDepartment.UnitID,DiDepartment.UnitName order by DiDepartment.UnitID";
+		List<String> unitNameList = new ArrayList<String>();
+		List<Integer> majorNum = new ArrayList<Integer>();
+		try{
+			st = conn.createStatement();
+			rs = st.executeQuery(sql);
+			while(rs.next()){
+				unitNameList.add(rs.getString("TeaUnit"));
+				majorNum.add(rs.getInt("majorNum"));
+			}
+		}catch (Exception e){
+			e.printStackTrace();
+			return null;
+		}	
+		int sum=0;
+		for(int i=0;i<majorNum.size();i++){
+			sum += majorNum.get(i);
+		}
+
+		String querysql="select FillUnitID,MajorName,MajorID,TotalCSHour,RequireCShour,OptionCSHour,InClassCSHour,ExpCSHour,PraCSHour," +
+				"TotalCredit,RequireCredit,OptionCredit,InClassCredit,ExpCredit,PraCredit,OutClassCredit from "+tableName1+" where Time like " +
+						" '"+year+"%' and FillUnitID like '3%' order by FillUnitID";
+		System.out.println(querysql);
+
+		List<A323_Bean> list = new ArrayList<A323_Bean>() ;
+		int count = 0, count1 = 0,i=0;
+		String unitName = null;
+		NumberFormat nf = NumberFormat.getNumberInstance();
+		nf.setMaximumFractionDigits(4);
+		int sum1 = 0,sum2 = 0,sum3 = 0,sum4 = 0,sum5 = 0,sum6 = 0,sum7 = 0,sum8 = 0,sum9 = 0,sum10 = 0,sum11 = 0,sumAll1 = 0,sumAll2 = 0;
+		try{
+			st = conn.createStatement() ;
+			rs = st.executeQuery(querysql) ;
+			while(i<sum){
+				
+				List<A323_Bean> list1 = new ArrayList<A323_Bean>() ;
+				int Total1=0,Num1=0,Num2=0,Num3=0,Num4=0,Num5=0;
+				double Total2=0,Num6=0,Num7=0,Num8=0,Num9=0,Num10=0,Num11=0;
+				int total1=0,num1=0,num2=0,num3=0,num4=0,num5=0;
+				double total2=0,num6=0,num7=0,num8=0,num9=0,num10=0,num11=0;
+				count = majorNum.get(count1);
+				for(int j=0;j<count;j++){
+					if(rs.next()){
+					i++;
+					A323_Bean bean = new A323_Bean ();
+					total1 = rs.getInt("TotalCSHour");
+					num1 = rs.getInt("RequireCShour");
+					num2 = rs.getInt("OptionCSHour");
+					num3 = rs.getInt("InClassCSHour");
+					num4 = rs.getInt("ExpCSHour");
+					num5 = rs.getInt("PraCSHour");
+
+					total2 = rs.getDouble("TotalCredit");	
+					num6 = rs.getDouble("RequireCredit");
+					num7 = rs.getDouble("OptionCredit");
+					num8 = rs.getDouble("InClassCredit");
+					num9 = rs.getDouble("ExpCredit");
+					num10 = rs.getDouble("PraCredit");
+					num11 = rs.getDouble("OutClassCredit");
+					
+					Total1 += total1;
+					Num1 += num1;
+					Num2 += num2;
+					Num3 += num3;
+					Num4 += num4;
+					Num5 += num5;
+					Total2 += total2;
+					Num6 += num6;
+					Num7 += num7;
+					Num8 += num8;
+					Num9 += num9;
+					Num10 += num10;
+					Num11 += num11;
+					unitName = rs.getString("MajorName");
+					bean.setTeaUnit(unitName);
+					bean.setUnitID(rs.getString("MajorID"));
+					bean.setRequireHour(Double.parseDouble(nf.format((double)num1/total1))*100);
+					bean.setOptionHour(Double.parseDouble(nf.format((double)num2/total1))*100);
+					bean.setInClassHour(Double.parseDouble(nf.format((double)num3/total1))*100);
+					bean.setExpHour(Double.parseDouble(nf.format((double)num4/total1))*100);
+					bean.setPraHour(Double.parseDouble(nf.format((double)num5/total1))*100);
+					
+					bean.setRequireCredit(Double.parseDouble(nf.format((double)num6/total2))*100);
+					bean.setOptionCredit(Double.parseDouble(nf.format((double)num7/total2))*100);
+					bean.setInClassCredit(Double.parseDouble(nf.format((double)num8/total2))*100);
+					bean.setExpCredit(Double.parseDouble(nf.format((double)num9/total2))*100);
+					bean.setPraCredit(Double.parseDouble(nf.format((double)num10/total2))*100);
+					bean.setOutClassCredit(Double.parseDouble(nf.format((double)num11/total2))*100);
+				    list1.add(bean);
+					}
+				}
+
+				    sum1 += Num1;
+				    sum2 += Num2;
+				    sum3 += Num3;
+				    sum4 += Num4;
+				    sum5 += Num5;
+				    sum6 += Num6;
+				    sum7 += Num7;
+				    sum8 += Num8;
+				    sum9 += Num9;
+				    sum10 += Num10;
+				    sum11 += Num11;
+				    sumAll1 += Total1;
+				    sumAll2 += Total2;
+					A323_Bean bean = new A323_Bean ();
+					bean.setTeaUnit(unitNameList.get(count1));
+					bean.setUnitID(null);
+					bean.setRequireHour(Double.parseDouble(nf.format((double)Num1/Total1))*100);
+					bean.setOptionHour(Double.parseDouble(nf.format((double)Num2/Total1))*100);
+					bean.setInClassHour(Double.parseDouble(nf.format((double)Num3/Total1))*100);
+					bean.setExpHour(Double.parseDouble(nf.format((double)Num4/Total1))*100);
+					bean.setPraHour(Double.parseDouble(nf.format((double)Num5/Total1))*100);
+					
+					bean.setRequireCredit(Double.parseDouble(nf.format((double)Num6/Total2))*100);
+					bean.setOptionCredit(Double.parseDouble(nf.format((double)Num7/Total2))*100);
+					bean.setInClassCredit(Double.parseDouble(nf.format((double)Num8/Total2))*100);
+					bean.setExpCredit(Double.parseDouble(nf.format((double)Num9/Total2))*100);
+					bean.setPraCredit(Double.parseDouble(nf.format((double)Num10/Total2))*100);
+					bean.setOutClassCredit(Double.parseDouble(nf.format((double)Num11/Total2))*100);
+					list.add(bean);
+					list.addAll(list1);
+					count1++;
+				}
+			A323_Bean bean = new A323_Bean ();
+			bean.setTeaUnit("全校合计");
+			bean.setUnitID(null);
+			bean.setRequireHour(Double.parseDouble(nf.format((double)sum1/sumAll1))*100);
+			bean.setOptionHour(Double.parseDouble(nf.format((double)sum2/sumAll1))*100);
+			bean.setInClassHour(Double.parseDouble(nf.format((double)sum3/sumAll1))*100);
+			bean.setExpHour(Double.parseDouble(nf.format((double)sum4/sumAll1))*100);
+			bean.setPraHour(Double.parseDouble(nf.format((double)sum5/sumAll1))*100);
+			
+			bean.setRequireCredit(Double.parseDouble(nf.format((double)sum6/sumAll2))*100);
+			bean.setOptionCredit(Double.parseDouble(nf.format((double)sum7/sumAll2))*100);
+			bean.setInClassCredit(Double.parseDouble(nf.format((double)sum8/sumAll2))*100);
+			bean.setExpCredit(Double.parseDouble(nf.format((double)sum9/sumAll2))*100);
+			bean.setPraCredit(Double.parseDouble(nf.format((double)sum10/sumAll2))*100);
+			bean.setOutClassCredit(Double.parseDouble(nf.format((double)sum11/sumAll2))*100);
+			list.add(0,bean);
+		
+			A323_DAO a323_Dao = new A323_DAO();
+			a323_Dao.save(list, year);
+		
+		}catch(Exception e){
+			e.printStackTrace() ;
+			return null;
+		}
+		
+		return list ;
+	}
+	
+
+	
+	public static void main(String arg[]){
+		String querysql="select FillUnitID,MajorName,MajorID,TotalCSHour,RequireCShour,OptionCSHour,InClassCSHour,ExpCSHour,PraCSHour," +
+		"TotalCredit,RequireCredit,OptionCredit,InClassCredit,ExpCredit,PraCredit,OutClassCredit from "+tableName1+" where Time like " +
+				" '"+2014+"%' and FillUnitID like '3%' order by FillUnitID";
+		System.out.println(querysql);
+	}
+
+}

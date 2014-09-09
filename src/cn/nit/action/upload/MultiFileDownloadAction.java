9149ee3da64f04e2ef0c89fd30ed82f0918ec7b1
scb <shengchaobo@126.com>
2014-09-09 21:12:15 +0800

    try
    Signed-off-by: scb <shengchaobo@126.com>

--- src/cn/nit/action/upload/MultiFileDownloadAction.java
+++ src/cn/nit/action/upload/MultiFileDownloadAction.java
@@ -99,6 +99,46 @@ public class MultiFileDownloadAction extends ActionSupport {
 		if(!J411_Excel.export_J411(filePath)){
 			return "J411_export has a error!";
 		}
+/*		if(!J412_Excel.export_J412(filePath)){
+			return "J412_export has a error!";
+		}*/
+/*		if(!J42_Excel.export_J42(filePath)){
+			return "J42_export has a error!";
+		}*/
+/*		if(!J43_Excel.export_J43(filePath)){
+			return "J43_export has a error!";
+		}*/
+/*		if(!J441_Excel.export_J441(filePath)){
+			return "J441_export has a error!";
+		}*/		
+/*		if(!J442_Excel.export_J442(filePath)){
+			return "J442_export has a error!";
+		}*/	
+/*		if(!J451_Excel.export_J451(filePath)){
+			return "J451_export has a error!";
+		}
+		if(!J452_Excel.export_J452(filePath)){
+			return "J452_export has a error!";
+		}
+		if(!J461_Excel.export_J461(filePath)){
+			return "J461_export has a error!";
+		}
+		if(!J462_Excel.export_J462(filePath)){
+			return "J461_export has a error!";
+		}
+		if(!J463_Excel.export_J463(filePath)){
+			return "J463_export has a error!";
+		}
+		if(!J464_Excel.export_J464(filePath)){
+			return "J464_export has a error!";
+		}
+		if(!J465_Excel.export_J465(filePath)){
+			return "J465_export has a error!";
+		}
+		if(!J466_Excel.export_J466(filePath)){
+			return "J466_export has a error!";
+		}*/
+
 		
 		return null;
 	}

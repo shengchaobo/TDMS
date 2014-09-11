6c885c98999f410cdcf26613433cb4d7846b92e0
FanShuangyan <12120410@bjtu.edu.cn>
2014-09-11 21:36:56 +0800

    J611-J616代码

---
+++ src/cn/nit/education/download/J615_Excel.java
@@ -0,0 +1,76 @@
+package cn.nit.education.download;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import cn.nit.bean.table6.T622_Bean;
+import cn.nit.dao.table6.T622_Dao;
+import cn.nit.util.ExcelUtil;
+
+public class J615_Excel {
+	
+	 public static boolean export_J615(String path){
+			
+			T622_Dao T622_dao = new T622_Dao();
+		
+			List<T622_Bean> list = T622_dao.getAllList("", null);
+			
+			String sheetName = "J-6-1-5近一届本科生录取标准及人数（时点）";
+			
+			List<String> columns = new ArrayList<String>();
+			columns.add("序号");
+			columns.add("省份");columns.add("批次");
+			columns.add("文科录取数");columns.add("理科录取数");
+			columns.add("文科批次最低控制线");columns.add("理科批次最低控制线");
+			columns.add("文科当年录取平均分");columns.add("理科当年录取平均分");
+			columns.add("说明");
+		
+			Map<String,Integer> maplist = new HashMap<String,Integer>();
+			maplist.put("SeqNum", 0);
+			maplist.put("Province", 1);maplist.put("Batch", 2);
+			maplist.put("LibEnrollNum", 3);maplist.put("SciEnrollNum", 4);
+			maplist.put("LibLowestScore", 5);maplist.put("SciLowestScore", 6);
+			maplist.put("LibAvgScore", 7);maplist.put("SciAvgScore", 8);
+			maplist.put("Note", 9);
+			
+			ByteArrayOutputStream byteArrayOutputStream = null;		
+			try {
+									
+				byteArrayOutputStream = ExcelUtil.exportExcel(list, sheetName, maplist,columns);
+
+				File file = new File(path,"J-6-1-5近一届本科生录取标准及人数（时点）.xls");
+				FileOutputStream fileOutputStream  = new FileOutputStream(file);
+				
+				//写到文件中
+				fileOutputStream.write(byteArrayOutputStream.toByteArray());
+				
+				byteArrayOutputStream.close();
+				fileOutputStream.close();
+				
+				return true;
+			} catch (Exception e) {	
+				e.printStackTrace();
+				return false;
+			}
+		}
+		
+		public static void main(String arg[]){
+			 String path = "D:\\江西项目\\相关表\\ExcelTest";
+			  J615_Excel excel = new J615_Excel();
+			  boolean flag = excel.export_J615(path);
+			  if(flag){
+				  System.out.println("成功！");
+			  }else{
+				  System.out.println("不成功！");
+			  }
+			}
+
+
+
+}
---
+++ src/cn/nit/education/download/J616_Excel.java
@@ -0,0 +1,68 @@
+package cn.nit.education.download;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import cn.nit.bean.table6.T621_Bean;
+import cn.nit.dao.table6.T621_Dao;
+import cn.nit.util.ExcelUtil;
+
+public class J616_Excel {
+	
+	 public static boolean export_J616(String path){
+			
+			T621_Dao T621_dao = new T621_Dao();
+		
+			List<T621_Bean> list = T621_dao.getAllList("", null);
+			
+			String sheetName = "J-6-1-6各专业招生报到情况（时点）";
+			
+			List<String> columns = new ArrayList<String>();
+			columns.add("序号");
+			columns.add("校内专业（大类）名称");columns.add("校内专业（大类）代码");
+			columns.add("招生计划数（人）");columns.add("实际录取数（人）");columns.add("实际报到数（人）");
+		
+			Map<String,Integer> maplist = new HashMap<String,Integer>();
+			maplist.put("SeqNum", 0);
+			maplist.put("majorName", 1);maplist.put("majorId", 2);
+			maplist.put("AmisPlanNum", 3);maplist.put("ActulEnrollNum", 4);maplist.put("ActulRegisterNum", 5);
+			
+			ByteArrayOutputStream byteArrayOutputStream = null;		
+			try {
+									
+				byteArrayOutputStream = ExcelUtil.exportExcel(list, sheetName, maplist,columns);
+
+				File file = new File(path,"J-6-1-6各专业招生报到情况（时点）.xls");
+				FileOutputStream fileOutputStream  = new FileOutputStream(file);
+				
+				//写到文件中
+				fileOutputStream.write(byteArrayOutputStream.toByteArray());
+				
+				byteArrayOutputStream.close();
+				fileOutputStream.close();
+				
+				return true;
+			} catch (Exception e) {	
+				e.printStackTrace();
+				return false;
+			}
+		}
+		
+		public static void main(String arg[]){
+			 String path = "D:\\江西项目\\相关表\\ExcelTest";
+			  J616_Excel excel = new J616_Excel();
+			  boolean flag = excel.export_J616(path);
+			  if(flag){
+				  System.out.println("成功！");
+			  }else{
+				  System.out.println("不成功！");
+			  }
+			}
+
+
+}
